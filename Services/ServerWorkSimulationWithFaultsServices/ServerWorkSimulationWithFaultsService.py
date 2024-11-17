1e06e782e807679bd4d2ca8e9da5def0675e10dd
Danijel Jovanović <danijel.xda@gmail.com>
2024-11-17 21:01:56 +0100

    prepare simulation algorithm

--- Services/ServerWorkSimulationWithFaultsServices/ServerWorkSimulationWithFaultsService.py
+++ Services/ServerWorkSimulationWithFaultsServices/ServerWorkSimulationWithFaultsService.py
@@ -15,3 +15,15 @@ def run_simulation_with_faults(users, posts, comments, servers: list[Server]):
 
     # Add fault to a server faulty list
     servers[faulty_server_index].dodaj_otkaz(int(fault["Id"].iloc[0]))
+
+    # 1st: Using random forest group up similar comments (answers)
+    # TODO
+
+    # 2nd: Using linear regression try to predict right comment (answer) for problem
+    # TODO
+
+    # If answer is found print comment (answer) and show which user wrote that comment
+    # TODO
+
+    # Otherwise print that server fault couldn't be solved right now
+    # TODO
