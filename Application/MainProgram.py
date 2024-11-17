99807f02aa620f31baca1763323eafe2637233f3
Danijel Jovanović <danijel.xda@gmail.com>
2024-11-17 19:26:30 +0100

    run periodic check

--- Application/MainProgram.py
+++ Application/MainProgram.py
@@ -1,4 +1,8 @@
+import threading
+import time
+
 from Domain.Models.Server import Server
+from Presentation.ServerStatusShow.ServerStatusUI import show_servers_status
 from Services.DataImporterServices.CommentsImporterService import load_comments_data
 from Services.DataImporterServices.PostsImporterService import load_posts_data
 from Services.DataImporterServices.UsersImporterService import load_users_data
@@ -7,7 +11,7 @@ from Services.DataPreprocessServices.PostsDataPreprocessService import preproces
 from Services.DataPreprocessServices.UserDataPreprocessService import preprocess_users_data
 
 # Load and data from XML
-users_df = load_users_data()
+""" users_df = load_users_data()
 posts_df = load_posts_data()
 comments_df = load_comments_data()
 
@@ -17,6 +21,8 @@ reputable_user_ids = users_data['AccountId'].tolist() # Extract reputable user I
 posts_data = preprocess_posts_data(posts_df, reputable_user_ids)
 comments_data = preprocess_comments_data(comments_df)
 
+"""
+
 # Make cluster of 10 Servers
 servers_cluster = [
     Server("Lenovo ThinkCentre MQ 7i"), Server("Dell OptiFlex 70D"), Server("HP OmniRun 2V"),
@@ -25,4 +31,6 @@ servers_cluster = [
     Server("HP ZBook Firefly G9"), Server("Lenovo Legion T5 28IMB05")
 ]
 
-[print(str(server)) for server in servers_cluster]
+# Show server statuses on every 5 seconds
+threading.Thread(target=lambda: [show_servers_status(servers_cluster) or time.sleep(15) for _ in iter(int, 1)]).start()
+
