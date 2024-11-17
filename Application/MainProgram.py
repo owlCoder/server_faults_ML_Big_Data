4fc06e8fdfe38069d1eb0ba19210f096a6297fa4
Danijel Jovanović <danijel.xda@gmail.com>
2024-11-17 19:29:36 +0100

    prepare simulation env

--- Application/MainProgram.py
+++ Application/MainProgram.py
@@ -11,7 +11,8 @@ from Services.DataPreprocessServices.PostsDataPreprocessService import preproces
 from Services.DataPreprocessServices.UserDataPreprocessService import preprocess_users_data
 
 # Load and data from XML
-""" users_df = load_users_data()
+""" 
+users_df = load_users_data()
 posts_df = load_posts_data()
 comments_df = load_comments_data()
 
@@ -20,7 +21,6 @@ users_data = preprocess_users_data(users_df)
 reputable_user_ids = users_data['AccountId'].tolist() # Extract reputable user IDs
 posts_data = preprocess_posts_data(posts_df, reputable_user_ids)
 comments_data = preprocess_comments_data(comments_df)
-
 """
 
 # Make cluster of 10 Servers
@@ -34,3 +34,8 @@ servers_cluster = [
 # Show server statuses on every 5 seconds
 threading.Thread(target=lambda: [show_servers_status(servers_cluster) or time.sleep(15) for _ in iter(int, 1)]).start()
 
+# Run simulation
+while 1:
+    print("simulation in progress")
+    time.sleep(10)
+    # todo dodaj simulaciju i neka prosledi sva 3 DF
